137acebed8fa8233072dca69d267054f087e05ef
CsharpSyntraWestKortrijk <61596529+CSharpSyntraWest@users.noreply.github.com>
2021-02-11 16:02:34 +0100

    Oplossing oefening 1 en 2

---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts
+{
+    public interface IRepositoryManager
+    {
+        IStudentRepository Student { get; }
+        ISubjectRepository Subject { get; }
+        IStudentSubjectRepository StudentSubject { get; }
+        void Save();
+    }
+}
---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Configurations;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Fluent API om mappings te definiëren tussen de model classes en de Database-tabellen
+            modelBuilder.Entity<Student>().Property(s => s.Id).HasDefaultValueSql("NEWID()");
+            modelBuilder.Entity<Subject>().Property(s => s.Id).HasDefaultValueSql("NEWID()");
+            modelBuilder.Entity<StudentSubject>().HasKey(sc => new { sc.StudentId, sc.SubjectId });//samengestelde PK in tabel StudentSubject
+
+            modelBuilder.Entity<StudentSubject>()
+                .HasOne<Student>(sc => sc.Student)
+                .WithMany(sc => sc.StudentSubjects)
+                .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder.Entity<StudentSubject>()
+                .HasOne<Subject>(sc => sc.Subject)
+                .WithMany(sc => sc.StudentSubjects)
+                .HasForeignKey(sc => sc.SubjectId);
+
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
+            modelBuilder.ApplyConfiguration(new SubjectConfiguration());
+            modelBuilder.ApplyConfiguration(new StudentSubjectConfiguration());
+        }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Subject> Subject { get; set; }
+        public DbSet<StudentSubject> StudentSubject { get; set; }
+
+    }
+}
